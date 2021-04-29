5a2fa1edad6408b9a93cf760b172069aad44c1b7
tttooommmooo <tabinn.0803.koyumayu@gmail.com>
2021-04-29 12:53:18 +0900

    db作成とマイグレーション編集
    rails db:creaate
    rails db:migrate

--- db/migrate/20210429034552_create_companies.rb
+++ db/migrate/20210429034552_create_companies.rb
@@ -1,7 +1,8 @@
 class CreateCompanies < ActiveRecord::Migration[6.0]
   def change
     create_table :companies do |t|
-
+      t.string :name
+      t.integer :rating_schedule 
       t.timestamps
     end
   end
